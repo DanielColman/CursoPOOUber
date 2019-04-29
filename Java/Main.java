a5c5072c9e63d571a9019017df3cbb17b5f50017
Daniel Colman <danielcolman@outlook.com>
2019-04-29 12:04:35 -0400

    Metodos Constructores Python and Java

--- Java/Main.java
+++ Java/Main.java
@@ -1,5 +1,10 @@
 class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo!");
+        Car car = new Car();
+        car.license = "ABC123";
+        car.driver = "Daniel Colman";
+        car.passenger = 4;
+        car.printDataCar();
     }
 }
--- Python/main.py
+++ Python/main.py
@@ -1,2 +1,8 @@
+from car import Car
+
 if __name__ == "__main__":
     print("Hola Mundo")
+    car = Car()
+    car.license = "ABC123"
+    car.driver = "Daniel Colman"
+    print(vars(car))
