8928e762f7ae4fe7ae3a16794b216c015c87193e
Daniel Colman <danielcolman@outlook.com>
2019-05-01 20:57:43 -0400

    Actualizando classes a EcmaScript 6 archivos JS

--- JS/Account.js
+++ JS/Account.js
@@ -1,9 +1,13 @@
-function Account (name, document) {
-    this.id;
-    this.name = name;
-    this.document = document;
-    this.email;
-    this.password;
-
-
+class Account {
+    
+    //Metodo constructor, con campos obligatorios
+    constructor(name, document){
+        this.id;
+        this.name = name;
+        this.document = document;
+        this.email;
+        this.password;
+    
+    }
+    
 }
